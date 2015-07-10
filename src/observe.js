8bd89bdd08935a655e9760217ee967c13c3aefc1
Nathan Sobo <nathan@github.com>
2015-07-10 17:00:20 -0500

    Automatically map over observation if a function is passed to observe

--- src/observe.js
+++ src/observe.js
@@ -2,6 +2,7 @@
 
 import ArrayObservation from './array-observation';
 
-export default function observe(object) {
-  return new ArrayObservation(object);
+export default function observe(object, fn) {
+  let observation = new ArrayObservation(object);
+  return fn ? observation.map(fn) : observation;
 }
